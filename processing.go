a96d88973dbc618213c96301feb39ae3ae240e8d
Alec Gleason <agleason@cisco.com>
2017-12-11 15:24:03 -0600

    Adding Processing for Canalyze Log format

--- processing.go
+++ processing.go
@@ -1,51 +1,51 @@
 package canlib
 
 import (
-    "golang.org/x/sys/unix"
-    "fmt"
-	"strings"
-	"strconv"
-	"encoding/hex"
+	"crypto/md5"
 	"encoding/binary"
-    "crypto/md5"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	"golang.org/x/sys/unix"
+	"strconv"
+	"strings"
 )
 
 // ByteArrayToCanFrame converts a byte array containing a CAN packet and converts it into a RawCanFrame
 func ByteArrayToCanFrame(array []byte, canMessage *RawCanFrame, captureTime int64, capIface string) {
-    canMessage.OID = binary.LittleEndian.Uint32(array[0:4])
-    canMessage.ID = canMessage.OID
-    canMessage.CaptureInterface = capIface
+	canMessage.OID = binary.LittleEndian.Uint32(array[0:4])
+	canMessage.ID = canMessage.OID
+	canMessage.CaptureInterface = capIface
 
-    // Check for the RTR Flag
-    if canMessage.ID & unix.CAN_RTR_FLAG != 0 {
-        canMessage.Rtr = true
-    }
+	// Check for the RTR Flag
+	if canMessage.ID&unix.CAN_RTR_FLAG != 0 {
+		canMessage.Rtr = true
+	}
 
-    // Check for the error flag
-    if canMessage.ID & unix.CAN_ERR_FLAG != 0 {
-        canMessage.Err = true
-    }
+	// Check for the error flag
+	if canMessage.ID&unix.CAN_ERR_FLAG != 0 {
+		canMessage.Err = true
+	}
 
-    // Check for extended can and adjust the ID accordingly
-    if canMessage.ID & unix.CAN_EFF_FLAG != 0 {
-        canMessage.Eff = true
-        canMessage.ID = canMessage.ID & unix.CAN_EFF_MASK
-    } else {
-        canMessage.Eff = false
-        canMessage.ID = canMessage.ID & unix.CAN_SFF_MASK
-    }
+	// Check for extended can and adjust the ID accordingly
+	if canMessage.ID&unix.CAN_EFF_FLAG != 0 {
+		canMessage.Eff = true
+		canMessage.ID = canMessage.ID & unix.CAN_EFF_MASK
+	} else {
+		canMessage.Eff = false
+		canMessage.ID = canMessage.ID & unix.CAN_SFF_MASK
+	}
 
-    canMessage.Dlc = array[4]
-    canMessage.Data = array[8:8+canMessage.Dlc]
-    canMessage.Timestamp = captureTime
+	canMessage.Dlc = array[4]
+	canMessage.Data = array[8 : 8+canMessage.Dlc]
+	canMessage.Timestamp = captureTime
 }
 
 // ProcessRawCan will process a raw can message to add additional contextual information
 func ProcessRawCan(processed *ProcessedCanFrame, frame RawCanFrame) {
-    processed.Packet = frame
-    toHash := append(frame.Data, byte(frame.ID))
-    processed.PacketHash = fmt.Sprintf("%x", md5.Sum(toHash))
+	processed.Packet = frame
+	toHash := append(frame.Data, byte(frame.ID))
+	processed.PacketHash = fmt.Sprintf("%x", md5.Sum(toHash))
 }
 
 // ProcessCandump will take a Socketcan/candump log and parse it into a raw_can_frame
@@ -77,5 +77,61 @@ func ProcessCandump(processed *RawCanFrame, frame string) error {
 		return errors.New("parsing data failed: " + err.Error())
 	}
 	processed.Dlc = uint8(binary.Size(processed.Data))
+	return nil
+}
+
+// ProcessCanalyzeLog will take a canalyze/canalyze-dump/can-dump log and parse it into a raw_can_frame
+func ProcessCanalyzeLog(processed *RawCanFrame, frame string) error {
+
+	splitSpaces := strings.Split(frame, " ")
+	var cleanSplit []string
+	for _, str := range splitSpaces {
+		if str != "" {
+			cleanSplit = append(cleanSplit, str)
+		}
+	}
+	processed.CaptureInterface = cleanSplit[0]
+	time, err := strconv.ParseFloat(cleanSplit[1], 64)
+	if err != nil {
+		return errors.New("parsing time failed: " + err.Error())
+	}
+	processed.Timestamp = int64(time * 1000000000)
+	oidInt, err := strconv.ParseUint(cleanSplit[2], 16, 32)
+	if err != nil {
+		return errors.New("parsing OID failed: " + err.Error())
+	}
+	processed.OID = uint32(oidInt)
+
+	idInt, err := strconv.ParseUint(cleanSplit[6], 16, 32)
+	if err != nil {
+		return errors.New("parsing ID failed: " + err.Error())
+	}
+	processed.ID = uint32(idInt)
+
+	dlcInt, err := strconv.ParseUint(cleanSplit[7], 10, 32)
+	if err != nil {
+		return errors.New("parsing DLC failed: " + err.Error())
+	}
+	processed.Dlc = uint8(dlcInt)
+	dataStr := strings.Join(cleanSplit[8:], "")
+	processed.Data, err = hex.DecodeString(dataStr)
+	if err != nil {
+		return errors.New("parsing data failed: " + err.Error())
+	}
+	if strings.Contains(cleanSplit[3], "NO") {
+		processed.Eff = false
+	} else {
+		processed.Eff = true
+	}
+	if strings.Contains(cleanSplit[4], "NO") {
+		processed.Rtr = false
+	} else {
+		processed.Rtr = true
+	}
+	if strings.Contains(cleanSplit[5], "NO") {
+		processed.Err = false
+	} else {
+		processed.Err = true
+	}
 	return nil
 }
--- processing_test.go
+++ processing_test.go
@@ -1,115 +1,141 @@
 package canlib
 
 import (
-    "testing"
-    "bytes"
+	"bytes"
+	"testing"
 )
 
 // TestByteArrayToCanFrame checks that ByteArrayToCanFrame accurately converts an Extended CAN frame into a RawCanFrame
 func TestByteArrayToCanFrame(t *testing.T) {
-    frame := []byte{109, 237, 19, 137, 8, 0, 0, 0, 15, 234, 197, 79, 101, 147, 251, 118}
-    expected := RawCanFrame{
-        OID: 2299784557,
-        ID: 152300909,
-        Rtr: false,
-        Err: false,
-        Eff: true,
-        Dlc: 8,
-        Data: []byte{15, 234, 197, 79, 101, 147, 251, 118},
-        CaptureInterface: "test",
+	frame := []byte{109, 237, 19, 137, 8, 0, 0, 0, 15, 234, 197, 79, 101, 147, 251, 118}
+	expected := RawCanFrame{
+		OID:              2299784557,
+		ID:               152300909,
+		Rtr:              false,
+		Err:              false,
+		Eff:              true,
+		Dlc:              8,
+		Data:             []byte{15, 234, 197, 79, 101, 147, 251, 118},
+		CaptureInterface: "test",
 	}
-    var result = new(RawCanFrame)
+	var result = new(RawCanFrame)
 	ByteArrayToCanFrame(frame, result, 0, "test")
-	if (result.OID != expected.OID) {
+	if result.OID != expected.OID {
 		t.Error("OID mismatch")
 	} else if result.ID != expected.ID {
 		t.Error("ID mismatch")
 	} else if result.Rtr != expected.Rtr {
 		t.Error("RTR mismatch")
 	} else if result.Err != expected.Err {
-        t.Error("ERR mismatch")
-    } else if result.Eff != expected.Eff {
-        t.Error("EFF mismatch")
-    } else if result.Dlc != expected.Dlc {
-        t.Error("data length mismatch")
-    } else if bytes.Equal(result.Data, expected.Data) != true {
-        t.Error("data value mismatch")
-    } else if result.CaptureInterface != expected.CaptureInterface {
-        t.Errorf("capture interface mismatch")
-    }
+		t.Error("ERR mismatch")
+	} else if result.Eff != expected.Eff {
+		t.Error("EFF mismatch")
+	} else if result.Dlc != expected.Dlc {
+		t.Error("data length mismatch")
+	} else if bytes.Equal(result.Data, expected.Data) != true {
+		t.Error("data value mismatch")
+	} else if result.CaptureInterface != expected.CaptureInterface {
+		t.Errorf("capture interface mismatch")
+	}
 }
 
 // BenchmarkByteArrayToCanFrame runs benchmarks on ByteArrayToCanFrame
 func BenchmarkByteArrayToCanFrame(b *testing.B) {
-    frame := []byte{109, 237, 19, 137, 8, 0, 0, 0, 15, 234, 197, 79, 101, 147, 251, 118}
-    var result = new(RawCanFrame)
-    b.ResetTimer()
+	frame := []byte{109, 237, 19, 137, 8, 0, 0, 0, 15, 234, 197, 79, 101, 147, 251, 118}
+	var result = new(RawCanFrame)
+	b.ResetTimer()
 	ByteArrayToCanFrame(frame, result, 0, "test")
-    for i := 0; i < b.N; i ++{
-        ByteArrayToCanFrame(frame, result, 0, "test")
-    }
+	for i := 0; i < b.N; i++ {
+		ByteArrayToCanFrame(frame, result, 0, "test")
+	}
 }
 
 // TestProcessRawCan will verify that can messages can be processed
 func TestProcessRawCan(t *testing.T) {
-    testFrame := RawCanFrame {
-        OID: 1,
-        ID: 1,
-        Rtr: false,
-        Eff: false,
-        Err: false,
-        Dlc: 1,
-        Data: []byte{1},
-    }
-    result := ProcessedCanFrame{}
-    ProcessRawCan(&result, testFrame)
-    expected := "249ba6277758050695e8f5909bacd6d3"
-    if result.PacketHash != expected {
-        t.Errorf("%s != %s", result.PacketHash, expected)
-    }
+	testFrame := RawCanFrame{
+		OID:  1,
+		ID:   1,
+		Rtr:  false,
+		Eff:  false,
+		Err:  false,
+		Dlc:  1,
+		Data: []byte{1},
+	}
+	result := ProcessedCanFrame{}
+	ProcessRawCan(&result, testFrame)
+	expected := "249ba6277758050695e8f5909bacd6d3"
+	if result.PacketHash != expected {
+		t.Errorf("%s != %s", result.PacketHash, expected)
+	}
 }
+
 // BenchmarkProcessRawCan runs benchmarks on ProcessRawCan
 func BenchmarkProcessRawCan(b *testing.B) {
-    testFrame := RawCanFrame {
-        OID: 1,
-        ID: 1,
-        Rtr: false,
-        Eff: false,
-        Err: false,
-        Dlc: 1,
-        Data: []byte{1},
-    }
-    result := ProcessedCanFrame{}
-    for i := 0; i < b.N; i ++{
-        ProcessRawCan(&result, testFrame)
-    }
+	testFrame := RawCanFrame{
+		OID:  1,
+		ID:   1,
+		Rtr:  false,
+		Eff:  false,
+		Err:  false,
+		Dlc:  1,
+		Data: []byte{1},
+	}
+	result := ProcessedCanFrame{}
+	for i := 0; i < b.N; i++ {
+		ProcessRawCan(&result, testFrame)
+	}
 }
 
 // TestProcessCandump will verify that candump messages are appropriately converted to rawcanframe
 func TestProcessCandump(t *testing.T) {
-    expected := RawCanFrame {
-        OID: 0,
-        ID: 1,
-        Rtr: false,
-        Eff: false,
-        Err: false,
-        Dlc: 1,
-        Data: []byte{1},
-        Timestamp: 1000000000,
-        CaptureInterface: "test",
-    }
-    result := RawCanFrame{}
-    ProcessCandump(&result, "(1) test 1#1")
-    if result.OID != expected.OID || expected.ID != result.ID {
-        t.Errorf("%s != %s", expected, result)
-    }
+	expected := RawCanFrame{
+		OID:              0,
+		ID:               1,
+		Rtr:              false,
+		Eff:              false,
+		Err:              false,
+		Dlc:              1,
+		Data:             []byte{1},
+		Timestamp:        1000000000,
+		CaptureInterface: "test",
+	}
+	result := RawCanFrame{}
+	ProcessCandump(&result, "(1) test 1#1")
+	if result.OID != expected.OID || expected.ID != result.ID {
+		t.Errorf("%s != %s", expected, result)
+	}
 }
 
 // BenchmarkProcessCandump runs benchmarks on ProcessCandump
 func BenchmarkProcessCandump(b *testing.B) {
-    result := RawCanFrame{}
-    b.ResetTimer()
-    for i := 0; i < b.N; i ++{
-        ProcessCandump(&result, "(1) test 1#1")
-    }
+	result := RawCanFrame{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ProcessCandump(&result, "(1) test 1#1")
+	}
+}
+
+// TestProcessCanalyze will verify that canalyze dump messages are appropriately converted to rawcanframe
+func TestProcessCanalyze(t *testing.T) {
+	example := "vcan0   1513023350.178403       1     NOEFF   NORTR   NOERR   1     2       21 22"
+	expected := RawCanFrame{
+		OID:              1,
+		ID:               1,
+		Rtr:              false,
+		Eff:              false,
+		Err:              false,
+		Dlc:              2,
+		Data:             []byte{33, 34},
+		Timestamp:        1513023350178402816,
+		CaptureInterface: "vcan0",
+	}
+	result := RawCanFrame{}
+	ProcessCanalyzeLog(&result, example)
+	if result.OID != expected.OID || result.ID != expected.ID || result.Dlc != expected.Dlc {
+		t.Errorf("%s != %s", expected, result)
+	}
+	if bytes.Compare(expected.Data, result.Data) != 0 {
+		t.Errorf("%s != %s", expected.Data, result.Data)
+	}
+
 }
